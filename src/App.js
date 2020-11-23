8d23661b2c83160a482fbabd18ade8cc16cc9160
Volodymyr <volodymyrquo@gmail.com>
2020-11-23 10:54:47 +0200

    done page srtucture app.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Cart, Default, Details, Navbar, ProductList } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={ProductList} />
+        <Route path='/details' component={Details} />
+        <Route path='/cart' component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as ProductList } from './ProductList/ProductList';
+export { default as Cart } from './Cart';
+export { default as Default } from './Default';
+export { default as Details } from './Details';
+export { default as Navbar } from './Navbar';
