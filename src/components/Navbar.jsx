59d317af1ce0128af093140ada5a4f4a5259bbd6
Volodymyr <volodymyrquo@gmail.com>
2020-11-23 11:46:31 +0200

    add styled components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import logo from '../logo.svg';
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className='navbar navbar-expand-sm navbar-dark px-sm-5'>
+      <nav className='navbar navbar-dark bg-primary navbar-expand-sm px-sm-5'>
         {/*  https://www.iconfinder.com/icons/1243689/call_phone_icon
     Creative Commons (Attribution 3.0 Unported);
     https://www.iconfinder.com/Makoto_msk */}
@@ -19,12 +20,34 @@ export default class Navbar extends Component {
           </li>
         </ul>
         <Link to='/cart' className='ml-auto'>
-          <button>
-            <i className='fas fa-cart-plus' />
+          <ButtonContainer>
+            <span className='mr-2'>
+              <i className='fas fa-cart-plus' />
+            </span>
             my cart
-          </button>
+          </ButtonContainer>
         </Link>
       </nav>
     );
   }
 }
+
+const ButtonContainer = styled.button`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--lightBlue);
+  color: var(--lightBlue);
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  cursor: pointer;
+  margin: 0.2rem 0.5rem 0.2rem 0;
+  transition: all 0.5s ease-in-out;
+  &:hover {
+    background: var(--lightBlue);
+    color: var(--mainBlue);
+  }
+  &:focus {
+    outline: none;
+  }
+`;
