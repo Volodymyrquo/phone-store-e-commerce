5dea74159ca032c7baaa6491f4cd426abba2049a
Volodymyr <volodymyrquo@gmail.com>
2020-11-28 06:52:46 +0200

    set PropTypes for Product

--- src/data.js
+++ src/data.js
@@ -3,11 +3,11 @@ export const storeProducts = [
     id: 1,
     title: 'Google Pixel - Black',
     img: 'img/product-1.png',
-    price: 10,
+    price: true,
     company: 'GOOGLE',
     info:
       'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
-    inCart: false,
+    inCart: '10',
     count: 0,
     total: 0,
   },
