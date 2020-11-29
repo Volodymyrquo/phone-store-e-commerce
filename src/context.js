1ea486e8af560abf7c2c7b5aa67b0cad1146a4f6
Volodymyr <volodymyrquo@gmail.com>
2020-11-29 04:44:55 +0200

    added func addTotals

--- src/context.js
+++ src/context.js
@@ -7,10 +7,10 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
-    cart: storeProducts,
+    cart: [],
     modalOpen: false,
     modalProduct: detailProduct,
-    cartSubTotal: 10,
+    cartSubTotal: 0,
     cartTax: 0,
     cartTotal: 0,
   };
@@ -48,7 +48,7 @@ class ProductProvider extends Component {
         return { products: tempProducts, cart: [...this.state.cart, product] };
       },
       () => {
-        console.log(this.state);
+        this.addTotals();
       }
     );
   };
@@ -75,6 +75,20 @@ class ProductProvider extends Component {
   };
   clearCart = () => {
     console.log(`cart was cleared`);
+  };
+  addTotals = () => {
+    let subTotal = 0;
+    this.state.cart.map((item) => (subTotal += item.total));
+    const tempTax = subTotal * 0.1;
+    const tax = parseFloat(tempTax.toFixed(2));
+    const total = subTotal + tax;
+    this.setState(() => {
+      return {
+        cartSubTotal: subTotal,
+        cartTax: tax,
+        cartTotal: total,
+      };
+    });
   };
   render() {
     return (
