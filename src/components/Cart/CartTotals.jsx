1e42e4db568115fb53fee25d72a53ea844c5a531
Volodymyr <volodymyrquo@gmail.com>
2020-11-29 10:02:32 +0200

    add paypal payment to project

--- src/components/Cart/CartTotals.jsx
+++ src/components/Cart/CartTotals.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PayPalButton from './PayPalButton';
 
-const CartTotals = ({ value }) => {
+const CartTotals = ({ value, history }) => {
   const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
   return (
     <>
@@ -27,6 +28,11 @@ const CartTotals = ({ value }) => {
               <span className='text-title'>total :</span>
               <strong>$ {cartTotal}</strong>
             </h5>
+            <PayPalButton
+              total={cartTotal}
+              clearCart={clearCart}
+              history={history}
+            />
           </div>
         </div>
       </div>
